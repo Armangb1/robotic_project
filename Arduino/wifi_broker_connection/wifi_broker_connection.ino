e7c3481aac4440c05f41e94410a5a4c66cd4c377
Arman <arman.ghbn@gmail.com>
2023-01-22 19:03:26 +0330

    wifi & MQTT connection finished

--- Arduino/wifi_broker_connection/wifi_broker_connection.ino
+++ Arduino/wifi_broker_connection/wifi_broker_connection.ino
@@ -7,79 +7,96 @@
 
 #define RX_PIN 2
 #define TX_PIN 3
-#define SSID_NAME "Xiaomi_0396" // put your SSID here
-#define WIFI_PASS "13801380"    // put your WIFI password here
-#define MQTT_USER "controller1" // put your MQTT username here
-#define MQTT_PASS "ABCDEFG"     // put your MQTT password here
+#define SSID_NAME "Irancell-TD-B5142-8304_1" // put your SSID here
+#define WIFI_PASS "z45i7cmy"                 // put your WIFI password here
+#define MQTT_USER "controller1"              // put your MQTT username here
+#define MQTT_PASS "12345678"                 // put your MQTT password here
+#define SERVER_IP "192.168.1.13"             // put your server IP here
+#define SERVER_PORT 1883                     // put your server port here
+
+void reconnect();
+void callback(char *topic, byte *payload, unsigned int length);
+void wifi_connect();
 
 SoftwareSerial ESPserial(RX_PIN, TX_PIN);
-int status = WL_IDLE_STATUS;
-
 WiFiEspClient ESPclient;
-IPAddress serverIP(192, 168, 31, 77); // server IP Address
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-PubSubClient MQTTclient(serverIP, 1883, callback, ESPclient);
 
-String MSG = "";
-String TOPIC = "";
+PubSubClient MQTTclient(SERVER_IP, SERVER_PORT, callback, ESPclient);
+
+String MSG;
+String TOPIC;
 
 void setup()
 {
-    // initialize serial for debugging
-    Serial.begin(115200);
-    // initialize serial for ESP module
-    ESPserial.begin(9600);
-    // initialize ESP module
-    WiFi.init(&ESPserial);
+  // initialize serial for debugging
+  Serial.begin(115200);
 
-    // attempt to connect to WiFi network
-    while (status != WL_CONNECTED)
-    {
-        Serial.print("Attempting to connect to WPA SSID: ");
-        Serial.println(SSID_NAME);
-        // Connect to WPA/WPA2 network
-        status = WiFi.begin(SSID_NAME, WIFI_PASS);
-    }
-    Serial.println("You're connected to the network");
-
-    // connect to MQTT broker
-    while (!MQTTclient.connected())
-    {
-        Serial.println("connection to server failed...");
-        Serial.println("attemping to reconnect...");
-        MQTTclient.connect("controller1", MQTT_USER, MQTT_PASS);
-        Serial.println(MQTTclient.state());
-    }
-    Serial.println("connected to the server");
-    MQTTclient.publish("topicout", "controller1 vasl shod");
-    MQTTclient.subscribe("topicin");
+  wifi_connect();
 }
 
 void loop()
 {
-    // enter your codde here
-
-    MQTTclient.loop();
+  if (!MQTTclient.connected())
+  {
+    reconnect();
+    // you can publish here for debug
+    MQTTclient.publish("topic", "hello from controller");
+    // subscribe here
+    MQTTclient.subscribe("topic");
+    //------------------------
+  }
+  MQTTclient.loop();
+  delay(10);
 }
 
-// void callback(char *topic, byte *payload, unsigned int length)
-// {
-//     Serial.print("Message arrived [");
-//     Serial.print(topic);
-//     TOPIC = topic;
-//     Serial.print("] ");
-//     for (int i = 0; i < length; i++)
-//     {
-//         Serial.print((char)payload[i]);
-//         MSG[i] = (char)payload[i];
-//     }
-//     Serial.println();
-// }
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!MQTTclient.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (MQTTclient.connect("arduinoClient", MQTT_USER, MQTT_PASS))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(MQTTclient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void wifi_connect()
+{
+  int status = WL_IDLE_STATUS;
+  // initialize serial for ESP module
+  ESPserial.begin(9600);
+  // initialize ESP module
+  WiFi.init(&ESPserial);
+
+  // attempt to connect to WiFi network
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(SSID_NAME);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(SSID_NAME, WIFI_PASS);
+  }
+  Serial.println("You're connected to the network");
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  MSG = "";
+  TOPIC = topic;
+  for (int i = 0; i < length; i++)
+  {
+    MSG += (char)payload[i];
+  }
+  Serial.println(TOPIC + " : " + MSG);
+}
