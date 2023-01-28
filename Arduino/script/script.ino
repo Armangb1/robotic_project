c45c38778000864560409513ef5b95251e8e4251
Arman <arman.ghbn@gmail.com>
2023-01-28 17:39:27 +0330

    arduino script is ready for coding

--- Arduino/script/script.ino
+++ Arduino/script/script.ino
@@ -1,6 +1,12 @@
 
 #include "C:/Users/Arman/Desktop/robotic-project/Arduino/libraries/Lib.h"
 
+// MQTT topics for the first controller
+const char gasTopic[] = "controller1/gas";
+const char lampTopic[] = "controller1/lamp";
+const char waterTopic[] = "controller1/water";
+const char lockTopic[] = "controller1/lock";
+
 void setup()
 {
   // initialize serial for debugging
@@ -11,11 +17,37 @@ void setup()
 
 void loop()
 {
+
   if (!MQTTclient.connected())
   {
     reconnect();
+    // subscribe to the topics
+    MQTTclient.subscribe(gasTopic);
+    MQTTclient.subscribe(lampTopic);
+    MQTTclient.subscribe(waterTopic);
+    MQTTclient.subscribe(lockTopic);
+  }
+
+  
+  if (TOPIC == gasTopic)
+  {
+    /* code */
+  }
+  else if (TOPIC == lampTopic)
+  {
+    /* code */
+  }
+  else if (TOPIC == waterTopic)
+  {
+    /* code */
+  }
+  else if (TOPIC == lockTopic)
+  {
+    /* code */
   }
 
   MQTTclient.loop();
   delay(10);
+  TOPIC = "";
+  MSG = "";
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+MQTT convention
+
+
+controller1_topic = {
+    gas: "controller1/gas",
+    lamp: "controller1/lamp",
+    water: "controller1/water",
+    lock: "controller1/lock",
+
+
+}
