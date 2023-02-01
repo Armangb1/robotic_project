92afb888b9c18dd52b3f9c92218ea540be3fb4ca
Arman <arman.ghbn@gmail.com>
2023-02-01 14:08:41 +0330

    assets updated

--- frontend/flutter/ui/lib/home/utils/app_data.dart
+++ frontend/flutter/ui/lib/home/utils/app_data.dart
@@ -1,13 +1,8 @@
 class AppData {
   static List smartDevices = [
     ['Smart Light', 'lib/home/assets/images/light-bulb.png', false, "lamp"],
-    [
-      'Smart Water valve',
-      'lib/home/assets/images/air-conditioner.png',
-      false,
-      "water"
-    ],
-    ['Smart Gas Valve', 'lib/home/assets/images/smart-tv.png', false, "gas"],
-    ['Smart Door Lock', 'lib/home/assets/images/fan.png', false, "door"],
+    ['Smart Water Tap', 'lib/home/assets/images/water.png', false, "water"],
+    ['Smart Gas Valve', 'lib/home/assets/images/gas.png', false, "gas"],
+    ['Smart Door Lock', 'lib/home/assets/images/lock.png', false, "lock"],
   ];
 }
